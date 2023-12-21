eff5c985ba9859c315ba47c91681ec892af79f67
ThiBaltM <thibalt.meurin@gmail.com>
2023-12-21 21:50:27 +0100

    mouvements

--- classCar.py
+++ classCar.py
@@ -9,9 +9,9 @@ class Car:
         self.screen = screen
         self.game = game
         self.image = pygame.transform.scale(pygame.image.load("assets/clio.png"),(100,100))
-        self.coords = (564,595)
+        self.coords = [564,595]
         self.angle = 0
-        self.speed =0
+        self.speed =5
 
 
 
@@ -19,12 +19,25 @@ class Car:
     def update(self):
         """Cette fonction met a jour les evenement divers pouvant avoir lieux"""
         self.screen.blit(self.image, (self.screen.get_width() /2 - self.image.get_width()/2,600))
+        self.coords[0] = self.coords[0] + math.cos(self.angle)*self.speed
+        self.coords[1] = self.coords[1] + math.sin(self.angle)*-self.speed
+
 
     def left(self):
-        self.angle -= math.pi/64
+        self.angle += math.pi/128
 
     def right(self):
-        self.angle += math.pi/64
+        self.angle -= math.pi/128
+    
+    def accelerate(self):
+        if(self.speed < 5):
+            self.speed +=0.1
+    
+    def brake(self):
+        if(self.speed>0):
+            self.speed -=0.2
+            if(self.speed < 0):
+                self.speed = 0
         
 
 
--- classGame.py
+++ classGame.py
@@ -15,7 +15,7 @@ class Game:
         self.screen = screen
         self.compteur = 0
         self.player = Car(self.screen, self)
-        self.pressed = {pygame.K_LEFT : False,pygame.K_RIGHT : False}
+        self.pressed = {pygame.K_LEFT : False,pygame.K_RIGHT : False, pygame.K_UP : False,pygame.K_DOWN : False}
         self.road = Road(self)
         self.camera = Camera(self.screen, self)
 
@@ -23,12 +23,16 @@ class Game:
     def update(self):
         """Cette fonction met a jour les evenement divers pouvant avoir lieux"""
         pygame.draw.rect(self.screen, "white", pygame.Rect(0,0,self.screen.get_width(), self.screen.get_height()))
-        self.player.update()
         self.camera.update()
+        self.player.update()
         if self.pressed[pygame.K_LEFT]:
             self.player.left()
         if(self.pressed[pygame.K_RIGHT]):
             self.player.right()
+        if(self.pressed[pygame.K_UP]):
+            self.player.accelerate()
+        if(self.pressed[pygame.K_DOWN]):
+            self.player.brake()
 
         self.road.showMinimap()
         
